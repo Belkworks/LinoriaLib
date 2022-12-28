973c5439f2cc3a24d0170a88ac9691f518e5e2ca
safazi <safazi@airmail.cc>
2022-12-28 14:57:04 -0600

    types

---
+++ src/index.d.ts
@@ -0,0 +1,121 @@
+// SIDES
+
+declare const enum Side {
+	Left = 1,
+	Right = 2,
+}
+
+type Sided = {
+	Side: Side;
+};
+
+// TODO: addons
+
+// TEXTBOX
+
+interface Textbox {
+	Value: string;
+	Type: "Input";
+
+	SetValue(value: string): void;
+	OnChanged(callback: Callback): void;
+}
+
+type TextboxInfo = {
+	Default?: string;
+	Text: string;
+	Placeholder?: string;
+	MaxLength?: number;
+};
+
+// TOGGLE
+
+interface Toggle {
+	Value: boolean;
+	Type: "Toggle";
+
+	OnChanged(callback: Callback): void;
+	SetValue(value: boolean): void;
+}
+
+type ToggleInfo = {
+	Default?: boolean;
+	Text: string;
+	BlankSize?: number;
+};
+
+// BASE GROUPBOX
+
+interface BaseGroupbox {
+	AddBlank(height: number): void;
+	AddLabel(text: string): void;
+	AddButton(text: string, callback: Callback): void;
+	AddInput(option: string, info: TextboxInfo): Textbox;
+	AddToggle(option: string, info: ToggleInfo): Toggle;
+	// TODO: AddSlider
+	// TODO: AddDropdown
+}
+
+// TAB BOX
+
+interface TabboxTab extends BaseGroupbox {
+	Show(): void;
+}
+
+interface Tabbox {
+	Tabs: TabboxTab[];
+
+	AddTab(name: string): TabboxTab;
+}
+
+type TabboxInfo = Sided & {
+	Name?: string;
+};
+
+// GROUP BOX
+
+interface Groupbox {}
+
+type GroupboxInfo = Sided & {
+	Name: string;
+};
+
+// TAB
+
+interface Tab {
+	Groupboxes: Record<string, Groupbox>;
+	Tabboxes: Record<string, Tabbox>;
+
+	ShowTab(): void;
+	AddGroupbox(info: GroupboxInfo): Groupbox;
+	AddLeftGroupbox(name: string): Groupbox;
+	AddRightGroupbox(name: string): Groupbox;
+	AddTabbox(info: TabboxInfo): Tabbox;
+	AddLeftTabbox(name?: string): Tabbox;
+	AddRightTabbox(name?: string): Tabbox;
+}
+
+// WINDOW
+
+interface Window {
+	Tabs: Tab[];
+
+	SetWindowTitle(title: string): void;
+	AddTab(title: string): Tab;
+}
+
+// LIBRARY
+
+interface Library {
+	Options: Record<string, Textbox>; // TODO: other types
+	Toggles: Record<string, Toggle>;
+
+	SetWatermarkVisibility(visible: boolean): void;
+	SetWatermark(text: string): void;
+	Notify(text: string, time?: number): void;
+	CreateWindow(title?: string): Window;
+}
+
+declare const Library: Library;
+
+export = Library;
